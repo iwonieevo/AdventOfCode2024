65f5d15b00e3b891b41c3dea7323a3196dd91b62
iwonieevo <280043@student.pwr.edu.pl>
2024-12-16 15:36:44 +0100

    added if __name__ == '__main__' for future's sake

--- day_1.py
+++ day_1.py
@@ -1,14 +1,15 @@
-first = []
-second = []
-print("Paste your input here (end input with empty line):")
-while True:
-    t = input()
-    if t != '':
-        t = t.split(' ')
-        first.append(int(t[0]))
-        second.append(int(t[-1]))
-    else:
-        break
+if __name__ == '__main__':
+    first = []
+    second = []
+    print("Paste your input here (end input with empty line):")
+    while True:
+        t = input()
+        if t != '':
+            t = t.split(' ')
+            first.append(int(t[0]))
+            second.append(int(t[-1]))
+        else:
+            break
 
-print(f"Part one: {sum([abs(f - s) for f, s in zip(sorted(first), sorted(second))])}")
-print(f"Part two: {sum([number * second.count(number) for number in first])}")
+    print(f"Part one: {sum([abs(f - s) for f, s in zip(sorted(first), sorted(second))])}")
+    print(f"Part two: {sum([number * second.count(number) for number in first])}")
